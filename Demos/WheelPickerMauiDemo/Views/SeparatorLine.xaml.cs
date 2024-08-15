f32782dcab0b39839c0da5357084c6a2193652eb
Benjamin Mayrargue <benjamin@vapolia.fr>
2024-08-15 10:06:08 +0200

    don't force the whole size

--- Demos/WheelPickerMauiDemo/Views/SeparatorLine.xaml.cs
+++ Demos/WheelPickerMauiDemo/Views/SeparatorLine.xaml.cs
@@ -5,7 +5,7 @@ public partial class SeparatorLine : ContentPage
     public string[][] MetricWeightItems { get; } =
     [
         ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
-        ["AA", "BB", "CC", "DD", "EE", "FF", "GG"]
+        //["AA", "BB", "CC", "DD", "EE", "FF", "GG"]
     ];
 
     public SeparatorLine()
